75b30a301f31466fe0077c5fdbf57ea0efdaf15e
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-10-13 15:01:08 -0400

    build(nix): create flake

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  inputs = {
+    ros2nix.url = "github:wentasah/ros2nix";
+
+    # important for ros overlay to work
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+  };
+  outputs =
+    {
+      self,
+      nix-ros-overlay,
+      ros2nix,
+      nixpkgs,
+    }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "Arcturus";
+          packages = [
+            pkgs.git
+            pkgs.bash
+            pkgs.colcon
+            pkgs.geographiclib
+            pkgs.python3Packages.rosdep
+            pkgs.python3Packages.scipy
+            pkgs.python3Packages.transforms3d
+            pkgs.python3Packages.torch
+            pkgs.SDL
+            ros2nix.packages.${pkgs.system}.ros2nix
+            # add other non-ROS packages here
+            (
+              with pkgs.rosPackages.humble;
+              buildEnv {
+                paths = [
+                  ros-core
+                  boost
+                  ament-cmake-core
+                  angles
+                  diagnostic-updater
+                  geographic-msgs
+                  message-filters
+                  tf-transformations
+                  tf2-ros-py
+                  tf2-eigen
+                  tf2-geometry-msgs
+                  yaml-cpp-vendor
+                  python-cmake-module
+                  rviz-common
+                  tf2-sensor-msgs
+                  mavros-msgs
+                  pcl-ros
+                  cv-bridge
+                  image-geometry
+                  rviz-satellite
+                  rviz-default-plugins
+                  rosbag2-py
+                  # add other ROS packages here
+                ];
+              }
+            )
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
