7010ac27dc8f0bbc6bcb4a5db0d1df83808a43c5
Youwen Wu <youwenw@gmail.com>
2025-01-29 20:56:53 -0800

    feat: generate source.sh with nix

--- bin/default.nix
+++ bin/default.nix
@@ -59,6 +59,39 @@ let
       echo -e "\e[1mUse \`./build.sh\` before running your first node.\e[0m"
     '';
 
+    source = writeShellScript "source.sh" ''
+      # main routine
+      main() {
+      	# source manifests
+      	cd dev_ws
+      	echo -e "\e[1mAdding all modules to ROS environment.\e[0m"
+      	dirs=($(
+      		cd install
+      		ls -d */ | sed 's:/$::'
+      	))
+      	for dir in "''${dirs[@]}"; do
+      		path="install/''${dir}/share/''${dir}/local_setup.bash"
+      		if [[ -f "$path" ]]; then
+      			source $path
+      		fi
+      	done
+      	echo "Done adding all modules to ROS environment."
+      	cd ..
+      }
+
+      # show help and exit
+      if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+      	echo -e "\e[1mUsage\e[0m: . ./source.sh [-h]"
+      	echo -e "\e[1mNote\e[0m: You can run nodes within a module with \`ros2 run module_name node.py\` after using this script."
+      	echo -e "\e[1mImportant\e[0m: Use a period (.) before the script name so that environment changes occur in your current shell session."
+      	echo -e "Sources necessary build files from ."
+      	echo -e "Run from root directory (dev_ws/install/ should be a subdirectory)."
+      else
+      	main
+      fi
+
+    '';
+
     # Run this script initially (in this directory) from inside a Nix development shell.
     # It will create a workspace `dev_ws` in the current directory.
     # Only run this script once and use `. ./run.sh` after.
